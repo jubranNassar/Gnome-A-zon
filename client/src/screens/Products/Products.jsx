7603a077ca5e3b964290688e0d5730a7f65643ca
mnebot <miguelnbt@icloud.com>
2021-07-21 10:27:47 -0400

    fix error for product screen to fix getGnomes import

--- client/src/screens/Products/Products.jsx
+++ client/src/screens/Products/Products.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { getGnomes } from "../src/services/gnomes.js";
+import { getGnomes } from "../../services/gnomes.js";
 import "./Products.css";
 
 const Products = (props) => {
