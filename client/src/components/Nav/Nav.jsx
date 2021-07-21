dd2dcac08a6c6bbc73cf9a177296163587eea536
mnebot <miguelnbt@icloud.com>
2021-07-21 15:55:54 -0400

    in nav added the authentication option

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,11 +1,30 @@
+import "./Nav.css";
+import { NavLink } from "react-router-dom";
+
+const authenticatedOption = (
+  <>
+    <NavLink to="/create">Rehome a Gnome</NavLink>
+    <NavLink to="/sign-out">Sign Out</NavLink>
+  </>
+);
+
+const unauthenticatedOptions = (
+  <>
+    <NavLink to="/sign-up">Sign Up</NavLink>
+    <NavLink to="/sign-in">Sign In</NavLink>
+  </>
+);
+const alwaysOption = (
+  <>
+    <NavLink to="/gnomes">Gnomes</NavLink>
+  </>
+);
 function Nav(props) {
   return (
     <div>
-      <nav>
-        NavLinks
-      </nav>
+      <nav>NavLinks</nav>
     </div>
-  )
+  );
 }
 
 export default Nav;
