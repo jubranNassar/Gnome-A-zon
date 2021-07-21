21c31b880e174550ed3c2c9f817bc7f772681b7a
yourusername <allwynavalos@allwyns-imac.myfiosgateway.com>
2021-07-21 12:28:17 -0400

    started the CSS for the home page

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -6,11 +6,11 @@ const Home = (props) => {
   return (
     <Layout user={props.user}>
       <div className='home'>
-        <div>
-        <h1> Welcome to Gnome-A-Zon!</h1>
-        <h2>Your source for garden fun and whimsey. Adopt a gnome or rehome your gnome today!</h2>
+        <div className="welcome_home_card">
+        <h1 className ="heading_home_text"> Welcome to Gnome-A-Zon!</h1>
+        <h2 className="subtext_home_text">Your source for garden fun and whimsey. Adopt a gnome or rehome your gnome today!</h2>
         </div>
-        <Link className='shop_gnomes_button' to={'/products'}><button> Shop Gnomes</button> </Link>
+        <Link to={'/gnomes'}><button> Shop Gnomes</button> </Link>
       </div>
     </Layout>
   )
