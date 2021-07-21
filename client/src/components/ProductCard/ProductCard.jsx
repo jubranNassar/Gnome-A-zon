8f2844a5d507c5299d514e1fd7f81ea853739d42
mnebot <miguelnbt@icloud.com>
2021-07-21 11:46:41 -0400

    add css to product card. width and color styling

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -1,11 +1,13 @@
-
+import "./ProductCard.css";
 
 const ProductCard = (props) => {
   return (
-    <div>
-     <img src={product.image_url} alt="" />
-          <h1>{product.name}</h1>
-          <p>{product.price}</p>
+    <div className="product-card">
+      <img src={props.image} alt="" />
+      <div className="product-card-text">
+        <p>{props.name}</p>
+        <p>{props.price}</p>
+      </div>
     </div>
   );
 };
