52a21d166d18bac8533ade7b87627500e46c9f93
girlwithcodes <heather.mantooth0@gmail.com>
2021-07-21 09:16:29 -0500

    Adds skeleton html to layout, header, nav, and footer components

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import Nav from '../Nav/Nav.jsx';
+
+function Header(props) {
+  return (
+    <div>
+      Header
+    </div>
+  )
+}
+
+export default Header;
---
+++ client/src/components/Nav/Nav.jsx
@@ -0,0 +1,11 @@
+function Nav(props) {
+  return (
+    <div>
+      <nav>
+        NavLinks
+      </nav>
+    </div>
+  )
+}
+
+export default Nav;
