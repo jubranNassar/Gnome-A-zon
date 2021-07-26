d4b56bb5576ed7138447955bbae823061cd1abbf
girlwithcodes <heather.mantooth0@gmail.com>
2021-07-26 10:14:50 -0500

    fixes but in ProductDetails

--- client/src/screens/ProductDetails/ProductDetails.jsx
+++ client/src/screens/ProductDetails/ProductDetails.jsx
@@ -7,7 +7,6 @@ import { useParams, Link, useHistory } from "react-router-dom";
 function GnomeDetails(props) {
   const [gnome, setGnome] = useState({});
   const [loaded, setLoaded] = useState(true);
-  const [seller, setSeller] = useState({});
   const { id } = useParams();
   const history = useHistory();
 
@@ -45,20 +44,16 @@ function GnomeDetails(props) {
       } else {
         return (
           <div className="seller-info-div">
-
-            <p>Sold by: {seller.username}</p>
-            <a href={`mailto: ${seller.email}`}>Email: {seller.email}</a>
-
-        
-
+            <p>Sold by: {gnome.seller.username}</p>
+            <a href={`mailto: ${gnome.seller.email}`}>Email: {gnome.seller.email}</a>
           </div>
         )
       } 
     } else if(gnome && Object.keys(gnome).length!==0) {
       return (
         <div className="seller-info-div">
-          <p>Sold by: {seller.username}</p>
-          <a href={"mailto: " +`${gnome.seller.email}`}>Email: {gnome.seller.email}</a>
+          <p>Sold by: {gnome.seller.username}</p>
+          <a href={`mailto: ${gnome.seller.email}`}>Email: {gnome.seller.email}</a>
         </div>
       )
     } else {
