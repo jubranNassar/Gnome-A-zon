4f4a61bde7a9b717b713e8daf3c3b4ce5749c533
girlwithcodes <heather.mantooth0@gmail.com>
2021-07-26 14:37:22 -0500

    Creates multiselect dropdown components for materials and category inputs and incorporates them into productCreate Screen

---
+++ client/src/components/CollectionSelect/CollectionSelect.jsx
@@ -0,0 +1,27 @@
+import Multiselect from 'multiselect-react-dropdown';
+import { useState } from 'react';
+
+function CollectionSelect({selectedCollections, setSelectedCollections}) {
+  
+    const handleSelect = (option) => {
+      console.log(option);
+      setSelectedCollections(...selectedCollections, option);
+    }
+  
+    return (
+      <div className="collection-select-div">
+        <Multiselect
+          isObject={false}
+          name = "category"
+          id = "category-input"
+          className="category-input-pc form-input-pc"
+          options = {['holiday', 'humorous', 'hobbies', 'traditional', 'cheeky', 'pop culture', 'other']}
+          onSelect = {handleSelect}
+          showCheckbox
+        />
+      </div>
+
+  )
+}
+
+export default CollectionSelect;
