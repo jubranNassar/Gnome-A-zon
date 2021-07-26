186b11cfa9f5ce14705031777be1c27a13402708
girlwithcodes <heather.mantooth0@gmail.com>
2021-07-26 10:05:38 -0500

    Fixes bug in ProductDetails component

--- client/src/screens/ProductDetails/ProductDetails.jsx
+++ client/src/screens/ProductDetails/ProductDetails.jsx
@@ -7,7 +7,6 @@ import { useParams, Link, useHistory } from "react-router-dom";
 function GnomeDetails(props) {
   const [gnome, setGnome] = useState({});
   const [loaded, setLoaded] = useState(true);
-  const [seller, setSeller] = useState({});
   const { id } = useParams();
   const history = useHistory();
 
@@ -30,6 +29,7 @@ function GnomeDetails(props) {
   const showUserOptions = () => {
     console.log(gnome);
     if(props.user && gnome && Object.keys(gnome).length!==0) {
+      console.log(props.user)
       if(props.user._userId===gnome.seller._id) {
         console.log(gnome);
         return (
@@ -46,15 +46,15 @@ function GnomeDetails(props) {
         return (
           <div className="seller-info-div">
 
-            <p>Sold by: {seller.username}</p>
-            <a href={`mailto: ${seller.email}`}>Email: {seller.email}</a>
+            <p>Sold by: {gnome.seller.username}</p>
+            <a href={`mailto: ${gnome.seller.email}`}>Email: {gnome.seller.email}</a>
           </div>
         )
       } 
     } else if(gnome && Object.keys(gnome).length!==0) {
       return (
         <div className="seller-info-div">
-          <p>Sold by: {seller.username}</p>
+          <p>Sold by: {gnome.seller.username}</p>
           <a href={`${gnome.seller.email}`}>Email: {gnome.seller.email}</a>
         </div>
       )
