34c134c2a7b5714018af3e36e53790241941cfa0
girlwithcodes <heather.mantooth0@gmail.com>
2021-07-27 10:38:30 -0500

    Adds styling to multiselect boxes

--- client/src/components/CollectionSelect/CollectionSelect.jsx
+++ client/src/components/CollectionSelect/CollectionSelect.jsx
@@ -1,8 +1,10 @@
 import Multiselect from 'multiselect-react-dropdown';
+import { useState } from 'react';
 
 function CollectionSelect({selectedCollections, setSelectedCollections}) {
   
     const handleSelect = (option) => {
+      console.log(option);
       setSelectedCollections(...selectedCollections, option);
     }
   
@@ -12,7 +14,7 @@ function CollectionSelect({selectedCollections, setSelectedCollections}) {
           isObject={false}
           name = "category"
           id = "category-input"
-          className="category-input-pc form-input-pc multiselect"
+          className="category-input-pc form-input-pc"
           options = {['holiday', 'humorous', 'hobbies', 'traditional', 'cheeky', 'pop culture', 'other']}
           onSelect = {handleSelect}
           showCheckbox
