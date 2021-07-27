5fe7905ba2270d811d1c00f73cc7eb164cd09e38
girlwithcodes <heather.mantooth0@gmail.com>
2021-07-27 14:52:28 -0500

    adds search bar to Products screen

--- client/src/screens/Products/Products.jsx
+++ client/src/screens/Products/Products.jsx
@@ -2,15 +2,18 @@ import { useState, useEffect } from "react";
 import { getGnomes } from "../../services/gnomes.js";
 import ProductCard from "../../components/ProductCard/ProductCard.jsx";
 import Layout from "../../components/Layout/Layout.jsx";
+import Search from "../../components/Search/Search.jsx";
 import "./Products.css";
 
 const Products = (props) => {
   const [products, setProducts] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
 
   useEffect(() => {
     const fetchProducts = async () => {
       const allProducts = await getGnomes();
       setProducts(allProducts);
+      setSearchResults(allProducts);
     };
     fetchProducts();
   }, []);
@@ -20,8 +23,9 @@ const Products = (props) => {
         <div className="product-heading">
           <p>Gnomes Seeking Homes</p>
         </div>
+        <Search products={products} setSearchResults={setSearchResults}/>
         <div className="products-container">
-          {products.map((product, index) => (
+          {searchResults.map((product, index) => (
             <ProductCard
               _id={product._id}
               image={product.image_url}
