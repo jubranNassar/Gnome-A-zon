6ec7ff560e4a5cd035b70f7aa28b4a8ebd7db805
girlwithcodes <heather.mantooth0@gmail.com>
2021-07-28 09:48:23 -0500

    Adds footer to Layout

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,5 +1,16 @@
+import './Footer.css';
+
 function Footer() {
-  return <div></div>;
+  return <div className="footer">
+    <div id="footer-site-description">
+      <p>Does your garden need a little spirit? A little humor? Maybe a little... magic?</p>
+      <p>Welcome to <span id="footer-site-name">Gnome-A-Zon!</span></p>
+      <p>Your garden gnome marketplace.</p>
+    </div>
+    <div id="footer-copyright">
+      <p>&copy; 2021 The Salty Gnomes</p>
+    </div>
+  </div>
 }
 
 export default Footer;
